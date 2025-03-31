c91697dbe25463c531dd2273f07aecdea5bd6eb7
Transfero <transfero.2025@senacrjedu.local>
2025-03-31 16:33:48 -0300

    enviando arquivos de hoje

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 
-from sistema.models import Filme, Genero, Usuario
+from sistema import models
 
 # Register your models here.
 
-@admin.register(Usuario)
+@admin.register(models.Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'sobrenome', 'cpf', 'telefone', 'email', 'endereco', 'data_cadastro', 'ativo', 'status')
     list_filter = ('ativo', 'status')
 
-@admin.register(Filme)
+@admin.register(models.Filme)
 class FilmeAdmin(admin.ModelAdmin):
     list_display = ('nome_do_filme', 'ano_lancamento', 'estudio', 'genero', 'sinopse', 'data_cadastro',)
     list_filter = ('ano_lancamento', 'estudio', 'genero')
 
-@admin.register(Genero)
+@admin.register(models.Genero)
 class GeneroAdmin(admin.ModelAdmin):
     list_display = ('nome_genero', 'data_cadastro',)
     list_filter = ('data_cadastro',)
--- sistema/models.py
+++ sistema/models.py
@@ -2,34 +2,45 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
 class Usuario(models.Model):
-    nome = models.CharField(max_length=100)
-    sobrenome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=14)
-    telefone = models.CharField(max_length=15)
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=20)
     email = models.EmailField()
     endereco = models.CharField(max_length=100)
-    data_cadastro = models.DateField()
-    ativo = models.BooleanField()
-    status = models.BooleanField()
+    data_cadastro = models.DateField(default=timezone.now)
+    ativo = models.BooleanField(default=True)
+    imagem = models.ImageField(blank=True, upload_to='imagens/%Y/%m')
+    status = models.BooleanField(default=True)
+
 
     def __str__(self):
-        return self.nome
+        return f'{self.nome} {self.sobrenome}'
+
+class Genero(models.Model):
+    nome_genero = models.CharField(max_length=50)
+    data_cadastro = models.DateField(default=timezone.now)
+    
+    def __str__(self):
+        return self.nome_genero
 
 class Filme(models.Model):
-    nome_do_filme = models.CharField(max_length=100)
-    ano_lancamento = models.DateField()
-    estudio = models.CharField(max_length=100)
-    genero = models.CharField(max_length=100)
+    nome_do_filme = models.CharField(max_length=50)
+    ano_lancamento = models.DateField(default=timezone.now)
+    estudio = models.CharField(max_length=50)
+    genero = models.CharField(max_length=50)
+    # genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True, blank=True)
     sinopse = models.TextField()
     data_cadastro = models.DateField(default=timezone.now)
     
     def __str__(self):
         return self.nome_do_filme
 
-class Genero(models.Model):
-    nome_genero = models.CharField(max_length=100)
-    data_cadastro = models.DateField(default=timezone.now)
+
     
-    def __str__(self):
-        return self.nome_genero
+
+
+# PK -> Primary Key -> Chave Primária.
+# FK -> Foreign Key -> Chave Estrangeira.
